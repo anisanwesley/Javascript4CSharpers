87230e1fd32526a0b76a6ba9c95bbb9288fc69fa
Wesley Lemos <anisan_wesley@live.com>
2015-10-09 16:03:23 -0300

    Exercicios jquery

---
+++ Exercícios/jQuery/eventos.js
@@ -0,0 +1,11 @@
+/*
+
+1. Adicione um evento no onChange do campo de pesquisa para alterar o conteúdo do botão abaixo para “Go to: <termo>”
+
+2. Crie um bind do evento click no item de lista que:
+	- Adicione uma classe "current" para o item de lista clicado.
+	- Remova a classe "current" do outro item de lista.
+
+*/
+
+
